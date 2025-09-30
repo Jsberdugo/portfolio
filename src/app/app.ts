5fc044ba44cdecb36bcab8d40d9c9aae422df415
Jsberdugo <juansiberdugo25@gmail.com>
2025-09-30 15:02:40 -0400

    feat: add cursor effect

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener, signal } from '@angular/core';
 import { IconsModule } from './shared/icons.module/icons.module-module';
 import { SobreMi } from './components/sobre-mi/sobre-mi';
 import { Experiencia } from './components/experiencia/experiencia';
@@ -11,7 +10,6 @@ import { Footer } from './components/footer/footer';
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
     IconsModule,
     SobreMi,
     Experiencia,
@@ -26,4 +24,55 @@ import { Footer } from './components/footer/footer';
 })
 export class App {
   protected readonly title = signal('portafolio');
+
+  private cursor!: HTMLElement;
+  private mouseX = 0;
+  private mouseY = 0;
+  private currentX = 0;
+  private currentY = 0;
+  private speed = 0.15; // delay suave
+  private sectionSpeeds: { [key: string]: number } = {
+    // lento
+    'sobre-mi': 1, // un poco más rápido
+    'yellow-section': 1,
+  };
+  ngAfterViewInit(): void {
+    this.cursor = document.getElementById('cursor')!;
+    this.animate();
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent): void {
+    this.mouseX = event.clientX;
+    this.mouseY = event.clientY;
+
+    // Detectar si pasa por sección amarilla
+    const el = document.elementFromPoint(event.clientX, event.clientY);
+    if (el && el.closest('.yellow-section')) {
+      this.cursor.classList.add('invert');
+    } else {
+      this.cursor.classList.remove('invert');
+    }
+
+    // Ajustar velocidad según sección detectada
+    if (el) {
+      for (const sectionClass in this.sectionSpeeds) {
+        if (el.closest(`.${sectionClass}`)) {
+          this.speed = this.sectionSpeeds[sectionClass];
+          break;
+        } else {
+          this.speed = 0.15;
+        }
+      }
+    }
+  }
+
+  private animate(): void {
+    this.currentX += (this.mouseX - this.currentX) * this.speed;
+    this.currentY += (this.mouseY - this.currentY) * this.speed;
+
+    this.cursor.style.transform = `translate(${this.currentX}px, ${this.currentY}px)`;
+
+    requestAnimationFrame(() => this.animate());
+  }
 }
